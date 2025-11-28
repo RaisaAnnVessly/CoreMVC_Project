eb645bddf75723ef2d81aedf64a5e4c521a34252
RaisaAnnVessly <raisaannvessly@gmail.com>
2025-11-28 18:35:02 +0530

    Admin Pageload modifiation and redirection after admin register

--- Core_Project/Controllers/AdminInsController.cs
+++ Core_Project/Controllers/AdminInsController.cs
@@ -31,7 +31,7 @@ namespace Core_Project.Controllers
             {
                 TempData["msg"] = ex.Message;
             }
-            return View("Admin_Pageload");
+            return View("Index");
         }
     }
 }
